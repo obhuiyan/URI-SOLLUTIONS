5b6e020cf99618e0cf387164370ea3991dd2264d
obhuiyan <84793254+obhuiyan@users.noreply.github.com>
2021-06-20 11:40:50 -0400

    Create URI 1011.c
    JUNE20 2021

---
+++ URI 1011.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    double r,pi,volume;
+    scanf("%lf",&r);
+    pi=3.14159;
+    volume=(4.0)/3*(pi*r*r*r);
+    printf("VOLUME = %.3lf\n",volume);
+    return 0;
+}
